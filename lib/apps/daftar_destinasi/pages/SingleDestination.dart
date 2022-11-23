103105406893cfb2911c6fecddb5c2871afa9419
eugeniusms <eugeniusms@gmail.com>
2022-11-23 23:41:49 +0700

    🔨 Style Detail Destination Page v1

--- lib/apps/daftar_destinasi/pages/SingleDestination.dart
+++ lib/apps/daftar_destinasi/pages/SingleDestination.dart
@@ -9,6 +9,7 @@ class SingleDestination extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 45, 48, 55),
       appBar: AppBar(
         title: Text("Detail"),
       ),
