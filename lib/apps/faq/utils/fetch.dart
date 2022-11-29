27dfc3f62f797689f87121dd3136394ad6ce7b81
Assajjade <abdillah.assajjad@ui.ac.id>
2022-11-29 16:24:06 +0700

    Abdi: Coba Fetch

---
+++ lib/apps/faq/utils/fetch.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:wisata_nusantara_mobile/apps/faq/models/public_faq_model.dart';
+
+class ToDoPage extends StatefulWidget {
+  const ToDoPage({Key? key}) : super(key: key);
+
+  @override
+  _ToDoPageState createState() => _ToDoPageState();
+}
+
+class _ToDoPageState extends State<ToDoPage> {
+  Future<List<PublicFaq>> fetchToDo() async {
+    var url =
+        Uri.parse('https://wisata-nusantara.up.railway.app/faq/json/public/');
+    var response = await http.get(
+      url,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+    );
+
+    // melakukan decode response menjadi bentuk json
+    var data = jsonDecode(utf8.decode(response.bodyBytes));
+
+    // melakukan konversi data json menjadi object ToDo
+    List<PublicFaq> listFaq = [];
+    for (var d in data) {
+      if (d != null) {
+        listFaq.add(PublicFaq.fromJson(d));
+      }
+    }
+
+    return listFaq;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('To Do'),
+        ),
+        body: FutureBuilder(
+            future: fetchToDo(),
+            builder: (context, AsyncSnapshot snapshot) {
+              if (snapshot.data == null) {
+                return const Center(child: CircularProgressIndicator());
+              } else {
+                if (!snapshot.hasData) {
+                  return Column(
+                    children: const [
+                      Text(
+                        "Tidak ada to do list :(",
+                        style:
+                            TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+                      ),
+                      SizedBox(height: 8),
+                    ],
+                  );
+                } else {
+                  return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (_, index) => Container(
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 12),
+                            padding: const EdgeInsets.all(20.0),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(15.0),
+                                boxShadow: const [
+                                  BoxShadow(
+                                      color: Colors.black, blurRadius: 2.0)
+                                ]),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "${snapshot.data![index].fields.question}",
+                                  style: const TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 10),
+                                Text("${snapshot.data![index].fields.answer}"),
+                              ],
+                            ),
+                          ));
+                }
+              }
+            }));
+  }
+}
