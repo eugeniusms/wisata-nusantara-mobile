573be68d8b9cb2f4f5b73978d60060ae0111fda4
aulianajwa <auliesl99@gmail.com>
2022-12-12 20:32:19 +0700

    styling login & register

--- lib/apps/authentication/pages/LoginPage.dart
+++ lib/apps/authentication/pages/LoginPage.dart
@@ -45,145 +45,182 @@ class _LoginPageState extends State<LoginPage> {
     final request = context.watch<CookieRequest>();
     // The rest of your widgets are down below
     Size size = MediaQuery.of(context).size;
-    return Form(
-        key: _loginFormKey,
-        child: Stack(
-          children: [
-            Scaffold(
-              backgroundColor: Colors.transparent,
-              body: Column(
-                children: [
-                  const Flexible(
-                    child: Center(
-                      child: Text(
-                        'Wisata Nusantara',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 60,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  Column(
+    return Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Colors.greenAccent, Colors.blueGrey])),
+        child: Form(
+            key: _loginFormKey,
+            child: Stack(
+              children: [
+                Scaffold(
+                  backgroundColor: Colors.transparent,
+                  body: Column(
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 25.0, vertical: 10.0),
-                        child: TextFormField(
-                          style: const TextStyle(color: Colors.white),
-                          decoration: InputDecoration(
-                            hintText: "Masukkan Username",
-                            labelText: "Username",
-                            labelStyle: const TextStyle(color: Colors.white),
-                            icon: const Icon(Icons.attach_email),
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5.0)),
-                            hintStyle: const TextStyle(color: Colors.white),
-                          ),
-                          onChanged: (String? value) {
-                            setState(() {
-                              username = value!;
-                            });
-                          },
-                          onSaved: (String? value) {
-                            setState(() {
-                              username = value!;
-                            });
-                          },
-                          autovalidateMode: AutovalidateMode.onUserInteraction,
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Username tidak boleh kosong';
-                            }
-                            return null;
-                          },
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 25.0, vertical: 10.0),
-                        child: TextFormField(
-                          obscureText: true,
-                          style: const TextStyle(color: Colors.white),
-                          decoration: InputDecoration(
-                            hintText: "Masukkan Password",
-                            labelText: "Password",
-                            labelStyle: const TextStyle(color: Colors.white),
-                            icon: const Icon(
-                              Icons.lock_outline,
-                            ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(5.0),
-                            ),
-                            hintStyle: const TextStyle(color: Colors.white),
-                          ),
-                          onChanged: (String? value) {
-                            setState(() {
-                              password1 = value!;
-                            });
-                          },
-                          onSaved: (String? value) {
-                            setState(() {
-                              password1 = value!;
-                            });
-                          },
-                          autovalidateMode: AutovalidateMode.onUserInteraction,
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Password tidak boleh kosong';
-                            }
-                            return null;
-                          },
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 25,
-                      ),
-                      Container(
-                        height: size.height * 0.08,
-                        width: size.width * 0.8,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16),
-                          color: const Color(0xFF24262A),
-                        ),
-                        child: TextButton(
-                          onPressed: () async {
-                            final response = await request.login(
-                                "https://wisata-nusa.up.railway.app/auth-flutter/login/",
-                                {
-                                  'username': username,
-                                  'password': password1,
-                                });
-                            if (response['status']) {
-                              UserLoggedIn.user = (User(username));
-                              ScaffoldMessenger.of(context)
-                                  .showSnackBar(const SnackBar(
-                                content: Text("Successfully logged in!"),
-                              ));
-                              Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => Dashboard(
-                                          title: "Dashboard",
-                                        )),
-                              );
-                              print(UserLoggedIn.user
-                                  .username); // GET USERNAME OF LOGGED IN USER
-                            } else {
-                              ScaffoldMessenger.of(context)
-                                  .showSnackBar(const SnackBar(
-                                content:
-                                    Text("An error occured, please try again."),
-                              ));
-                            }
-                          },
-                          child: const Text(
-                            'Submit',
+                      const Flexible(
+                        child: Center(
+                          child: Text(
+                            'Wisata Nusantara',
+                            textAlign: TextAlign.center,
                             style: TextStyle(
-                                fontSize: 22,
                                 color: Colors.white,
-                                height: 1.5,
+                                fontSize: 60,
                                 fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 25.0, vertical: 10.0),
+                            child: TextFormField(
+                              style: const TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                hintText: "Masukkan Username",
+                                labelText: "Username",
+                                labelStyle:
+                                    const TextStyle(color: Colors.white),
+                                icon: const Icon(Icons.attach_email),
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(5.0)),
+                                hintStyle: const TextStyle(color: Colors.white),
+                              ),
+                              onChanged: (String? value) {
+                                setState(() {
+                                  username = value!;
+                                });
+                              },
+                              onSaved: (String? value) {
+                                setState(() {
+                                  username = value!;
+                                });
+                              },
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Username tidak boleh kosong';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 25.0, vertical: 10.0),
+                            child: TextFormField(
+                              obscureText: true,
+                              style: const TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                hintText: "Masukkan Password",
+                                labelText: "Password",
+                                labelStyle:
+                                    const TextStyle(color: Colors.white),
+                                icon: const Icon(
+                                  Icons.lock_outline,
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5.0),
+                                ),
+                                hintStyle: const TextStyle(color: Colors.white),
+                              ),
+                              onChanged: (String? value) {
+                                setState(() {
+                                  password1 = value!;
+                                });
+                              },
+                              onSaved: (String? value) {
+                                setState(() {
+                                  password1 = value!;
+                                });
+                              },
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Password tidak boleh kosong';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 25,
+                          ),
+                          Container(
+                            height: size.height * 0.08,
+                            width: size.width * 0.8,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16),
+                              color: const Color(0xFF24262A),
+                            ),
+                            child: TextButton(
+                              onPressed: () async {
+                                final response = await request.login(
+                                    "https://wisata-nusa.up.railway.app/auth-flutter/login/",
+                                    {
+                                      'username': username,
+                                      'password': password1,
+                                    });
+                                if (response['status']) {
+                                  UserLoggedIn.user = (User(username));
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(const SnackBar(
+                                    content: Text("Successfully logged in!"),
+                                  ));
+                                  Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Dashboard(
+                                              title: "Dashboard",
+                                            )),
+                                  );
+                                  print(UserLoggedIn.user
+                                      .username); // GET USERNAME OF LOGGED IN USER
+                                } else {
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(const SnackBar(
+                                    content: Text(
+                                        "An error occured, please try again."),
+                                  ));
+                                }
+                              },
+                              child: const Text(
+                                'Submit',
+                                style: TextStyle(
+                                    fontSize: 22,
+                                    color: Colors.white,
+                                    height: 1.5,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 35,
+                          ),
+                        ],
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          // Route menu ke counter
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const RegisterPage()),
+                          );
+                        },
+                        child: Container(
+                          decoration: const BoxDecoration(
+                              border: Border(
+                                  bottom: BorderSide(
+                                      width: 1, color: Colors.white))),
+                          child: const Text(
+                            'Create New Account',
+                            style: TextStyle(
+                                fontSize: 22, color: Colors.white, height: 1.5),
                           ),
                         ),
                       ),
@@ -192,34 +229,8 @@ class _LoginPageState extends State<LoginPage> {
                       ),
                     ],
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      // Route menu ke counter
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const RegisterPage()),
-                      );
-                    },
-                    child: Container(
-                      decoration: const BoxDecoration(
-                          border: Border(
-                              bottom:
-                                  BorderSide(width: 1, color: Colors.white))),
-                      child: const Text(
-                        'Create New Account',
-                        style: TextStyle(
-                            fontSize: 22, color: Colors.white, height: 1.5),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 35,
-                  ),
-                ],
-              ),
-            )
-          ],
-        ));
+                )
+              ],
+            )));
   }
 }
