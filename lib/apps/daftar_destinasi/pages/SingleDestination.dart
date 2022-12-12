02acc211a2f73d540213924a4b04a9022b44f33c
eugeniusms <eugeniusms@gmail.com>
2022-12-12 18:23:50 +0700

    🔧 Style Single Destination Clear

--- lib/apps/daftar_destinasi/pages/SingleDestination.dart
+++ lib/apps/daftar_destinasi/pages/SingleDestination.dart
@@ -13,8 +13,20 @@ class SingleDestination extends StatelessWidget {
       appBar: AppBar(
         title: Text("Detail"),
       ),
-      body: Container(
-        padding: EdgeInsets.all(12.0),
+        body: Container( 
+          decoration: BoxDecoration(
+              color: Colors.green[100],
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.all(Radius.circular(0.0))),
+          padding: const EdgeInsets.only(
+              right: 20.0, left: 20.0, top: 35.0, bottom: 60.0),
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.all(Radius.circular(30.0))),
+            padding: EdgeInsets.only(
+                right: 40.0, left: 40.0, top: 20.0, bottom: 20.0),
         child: Column(
           children: <Widget>[
             Container(
@@ -30,60 +42,109 @@ class SingleDestination extends StatelessWidget {
               borderRadius: BorderRadius.circular(20.0),
               child: Image.network(
                 data.foto_cover_url,
-                width: 400,
+                    width: 300,
               ),
             ),
             const SizedBox(height: 20),
             Align(
+                    
                 child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Row(children: [
-                  Text("Location: ",
+                    // ============================= LOCATION ================================
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(children: [
+                                  Icon(Icons.location_on,
+                                      color: Colors.green[600]),
+                                  Text("Location ",
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w700,
+                                          color: Colors.green[600]))
+                                ]),
+                                Row(children: [
+                                  Icon(Icons.favorite, color: Colors.red[600]),
+                                  Text("${data.jumlah_suka}",
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w700)),
+                                  Text(" likes",
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w700,
+                                          color: Colors.black))
+                                ])
+                              ]),
+                          Container(
+                              padding: EdgeInsets.all(10.0),
+                              decoration: BoxDecoration(
+                                  color: Colors.grey[100],
+                                  borderRadius: BorderRadius.circular(10.0)),
+                              child: Text("${data.lokasi}, Indonesia",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w700,
+                                  )))
+                        ]),
+                    // ============================= CATEGORY ================================
+                    SizedBox(height: 10),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(children: [
+                            Icon(Icons.category, color: Colors.orange[600]),
+                            Text("Category ",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w700,
+                                    color: Colors.orange[600]))
+                          ]),
+                          Container(
+                              padding: EdgeInsets.all(10.0),
+                              decoration: BoxDecoration(
+                                  color: Colors.grey[100],
+                                  borderRadius: BorderRadius.circular(10.0)),
+                              child: Text("${data.kategori}",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w700,
+                                  )))
+                        ]),
+                    // ============================= DESCRIPTION ================================
+                    SizedBox(height: 10),
+
+                    Container(
+                        padding: EdgeInsets.all(10.0),
+                        decoration: BoxDecoration(
+                            color: Colors.grey[100],
+                            borderRadius: BorderRadius.circular(10.0)),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Description",
                       style:
                           TextStyle(
                           fontSize: 16,
                           fontWeight: FontWeight.w700,
-                          color: Colors.black)),
-                  Text("${data.lokasi}, Indonesia",
-                      style: TextStyle(fontSize: 16, color: Colors.black))
-                ]),
-                Row(children: [
-                  Text("Category: ",
-                      style:
-                          TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w700,
-                          color: Colors.black)),
-                  Text("${data.kategori}",
-                      style: TextStyle(fontSize: 16, color: Colors.black))
-                ]),
-                Row(children: [
-                  Text("Like: ",
-                      style:
-                          TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w700,
-                          color: Colors.black)),
-                  Text("${data.jumlah_suka}",
-                      style: TextStyle(fontSize: 16, color: Colors.black))
-                ]),
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                  Text("Description: ",
-                      style:
-                          TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w700,
-                          color: Colors.black)),
+                                      color: Colors.black)),
+                              SizedBox(height: 10),
                   Text("${data.deskripsi}",
                       style: TextStyle(fontSize: 16, color: Colors.black))
-                ]),
+                            ])),
               ],
             ))
           ],
         ),
       ),
-    );
+        ));
   }
 }
 
